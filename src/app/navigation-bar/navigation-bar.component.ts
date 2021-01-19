f71dbdbddd30fd69aa07230acb08ebf20231909a
raghu <medishettyraghu0302@gmail.com>
2021-01-19 07:07:33 -0400

    read me

---
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+	selector: 'navigaiton-bar',
+	templateUrl: './navigation-bar.component.html'
+})
+
+export class NavigationBar implements OnInit {
+
+constructor() { }
+ ngOnInit(): void {
+  }	
+}
